d97babad052507b54b32fbb4baeb97fe859758b2
Ade Nugroho <149552847+adenugii@users.noreply.github.com>
2025-05-29 19:56:34 +0700

    Revert "Profile Setup & Configure Database"

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,7 @@
-// next.config.js
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.spoonacular.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'serpapi.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.gstatic.com', // Covers encrypted-tbn0.gstatic.com, encrypted-tbn1.gstatic.com, etc.
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.shopee.co.id',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.tokopedia.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.blibli.com',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
